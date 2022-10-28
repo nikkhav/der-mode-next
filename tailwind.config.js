30e52000d7edd5e49fbc1d399644b719db073666
nikkhav <103598295+nikkhav@users.noreply.github.com>
2022-10-28 18:00:46 +0200

    Fixing bugs 4

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./layout/**/*.{js,ts,jsx,tsx}",
+    "./styles/**/*.{js,ts,jsx,tsx}",
+    "./lib/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     fontFamily: {
